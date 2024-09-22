d7f73b5b8f5a0b5cb8bc47b7a2fd7570325146ba
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-09-22 13:36:32 +0300

    my commits

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+rows = int(input("Enter the size of the pattern: "))
+
+while rows:
+    for i in range (0,rows):
+
+        for j in range (0,rows):
+            print("*", end="")
+        print()
+    break
