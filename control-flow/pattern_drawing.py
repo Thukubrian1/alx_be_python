8a3e7cc54c015bb12aed0acf7d1e663e7301764c
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-09-22 13:55:20 +0300

    Revert "my commits"
    This reverts commit 1e79555f423de0ed60be34f15f366a7c44845af1.

--- control-flow/pattern_drawing.py
+++
@@ -1,9 +0,0 @@
-rows = int(input("Enter the size of the pattern: "))
-
-while rows:
-    for i in range (0,rows):
-
-        for j in range (0,rows):
-            print("*", end="")
-        print()
-    break
