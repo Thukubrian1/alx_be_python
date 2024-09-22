f292efbded44754bba3cb71a93f90fea85da431b
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-09-22 17:15:09 +0300

    daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,29 @@
-task = input("Input your task: ")
+Task = input("Input your task: ")
 
-priority = input("Priority (high/medium/low): ")
+Priority = input("Priority (high/medium/low): ")
 
-time_bound = input("Is it time-bound? (yes/no): ")
+Time_bound = input("Is it time-bound? (yes/no): ")
 
-match priority:
+match Priority:
     case "high":
-        if time_bound == "yes":
-            print(f"{task} is a high priority task that requires immediate attention today! ")
+        if Time_bound == "yes":
+            print(f"{Task} is a high priority task that requires immediate attention today! ")
         
-        elif time_bound == "no":
-            print(f"{task} is a high priority task. Consider completing it when you have free time ")
+        elif Time_bound == "no":
+            print(f"{Task} is a high priority task. Consider completing it when you have free time ")
     
     case "medium":
-        if time_bound == "yes":
-            print(f"{task} is a medium priority task that requires immediate attention today! ")
+        if Time_bound == "yes":
+            print(f"{Task} is a medium priority task that requires immediate attention today! ")
         
-        elif time_bound == "no":
-            print(f"{task} is a medium priority task. Consider completing it when you have free time ")
+        elif Time_bound == "no":
+            print(f"{Task} is a medium priority task. Consider completing it when you have free time ")
 
     case "low":
-        if time_bound:
+        if Time_bound:
             print(f"{task} is a low priority task that requires immediate attention today! ")
         
-        elif time_bound == "no":
-            print(f"{task} is a low priority task. Consider completing it when you have free time ")
+        elif Time_bound == "no":
+            print(f"{Task} is a low priority task. Consider completing it when you have free time ")
     case _:
         print("Invalid input")
