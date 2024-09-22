0f9133e2e7405965f2e8d61e558993011fd35f21
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-09-22 14:17:59 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 weather = "sunny" or "rainy" or "cold"
 
-weather = input("What's the weather like today? (sunny/rainy/cold) ")
+weather = input( "What's the weather like today ? (sunny /rainy /cold ) ")
 
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
