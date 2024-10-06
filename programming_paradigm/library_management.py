440cdd8bcfc95ed96974643811f8604957b70043
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-10-06 22:24:07 +0300

    Library Systemr

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -16,22 +16,22 @@ class Book:
 
 class Library(Book):
     def __init__(self):
-        self._book = []
+        self._books = []
 
     def __init__(self , title, author, is_checked_out , book):
         super().__init__(title, author, is_checked_out)
-        self._book = book
+        self._books = book
         
     def add_book(self):
-        self._book.append(self._book)
+        self._books.append(self._books)
         print(f"You have added {self.title} by {self.author}")
 
     def checkout_book(self):
-        self.checkout_book.remove(self._book)
+        self.checkout_book.remove(self._books)
         return f"Available books after checking out '1984'\n {self.title}"
 
     def return_book(self):
-        self.return_book.append(self._book)
+        self.return_book.append(self._books)
         return f"Available books after returing '1984':\n {self.title}\n {self.author}"
 
     def list_available_books(self):
