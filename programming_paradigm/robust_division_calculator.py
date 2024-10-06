88a5ae5ab3d31cc314d1f09fb7e779d0d58846d8
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-10-06 19:51:45 +0300

    robust division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -9,4 +9,4 @@ def safe_divide(numerator , denominator):
         return "Error: Cannot divide by zero."
     
     except  ValueError:
-        return "Error: Enter a numeric value"
+        return "Error: Please enter numeric values only."
