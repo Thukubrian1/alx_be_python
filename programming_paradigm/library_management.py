51e266922700ce48713631b3bd9f1bd34efe54c0
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-10-06 21:20:20 +0300

    robust division calculator

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self,title,author,is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = is_checked_out
+
+class Library(Book):
+    def __init__(self , title, author, is_checked_out , book):
+        super().__init__(title, author, is_checked_out)
+        self._book = book
+
+    def add_book(self,book):
+        self.add_book += book
+        print(f"You have added {self.title} by {self.author}")
+
+    def checkout_book(self,book):
+        self.checkout_book -= book
+        return f"Available books after checking out '1984'\n {self.title}"
+
+    def return_book(self,book):
+        self.return_book += book
+        return f"Available books after returing '1984':\n {self.title}\n {self.author}"
+
+    def list_available_books(self):
+        return f"Available books after setup:\n {self.title}\n {self.author}"
